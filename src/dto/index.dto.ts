7f68ddd619dec013a2f575aebab077a831a26386
Thien Phuc Khuc <khucthienphuc@gmail.com>
2023-01-02 17:30:02 +0700

    feat: add docker feature

--- src/dto/index.dto.ts
+++ src/dto/index.dto.ts
@@ -1,2 +1,6 @@
-export * from '../users/user-dto/user.dto';
 export * from './common.dto';
+export * from './upload.dto';
+export * from '../users/user-dto/user.dto';
+export * from '../movie/movie-dto/movie.dto';
+export * from '../theatre/theatre-dto/theatre.dto';
+export * from '../booking/booking-dto/booking.dto';
